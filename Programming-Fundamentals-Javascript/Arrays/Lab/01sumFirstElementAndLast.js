1f515f2cdfbeb4bc7b637eac27eec968cf1995e4
PetraGeorgieva <Petra.kirilova.georgieva@gmail.com>
2023-04-03 13:07:43 +0200

    new lessons array

---
+++ Programming-Fundamentals-Javascript/Arrays/Lab/01sumFirstElementAndLast.js
@@ -0,0 +1,9 @@
+function sumOfElements (array) {
+  let firstElemnt = array[0]
+  let lastelement = array[array.length -1]
+  let sum = firstElemnt + lastelement
+  console.log(sum)
+}
+let array = [20,30,40]
+sumOfElements(array)
+sumOfElements([10,17,22,33])
---
+++ Programming-Fundamentals-Javascript/Arrays/Lab/02dayOfWeek.js
@@ -0,0 +1,17 @@
+function daysofweeks(n) {
+   let days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+   ]
+   if (n >1&& n < 7){
+    console.log(days[ n -1])
+   } else  {
+    console.log('Invalid day!')
+   }
+}
+daysofweeks(8)
